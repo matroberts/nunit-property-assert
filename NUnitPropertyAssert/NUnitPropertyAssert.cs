18f99067de8bd6d295c16b3f3499b88ed7394621
mat roberts <mat.roberts@biomni.com>
2017-05-25 15:26:04 +0100

    added empty project

---
+++ NUnitPropertyAssert/NUnitPropertyAssert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NUnitPropertyAssert
+{
+    [TestFixture]
+    public class NUnitPropertyAssert
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
